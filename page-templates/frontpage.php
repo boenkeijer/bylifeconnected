fca10b350c047632903b69f415eacb6d7cb4d26e
boenkeijer <kebeijer@gmail.com>
2020-05-08 15:01:21 +0200

    Create frontpage.php

---
+++ page-templates/frontpage.php
@@ -0,0 +1,20 @@
+<?php
+  /* Template Name: Frontpage */ 
+?>
+
+<?php get_header(); ?>
+
+<main class="frontpage">  
+  	<section>  
+		<h1><?php the_title(); ?></h1>
+		<ul class="sub-page">
+			<li>What we do</li>
+			<li>About us</li>
+			<li>Our partners</li>
+			<li>News</li>
+			<li>What you can do</li>
+		</ul>    	
+	</section>
+</main>
+
+<?php get_footer(); ?>
