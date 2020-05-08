f995b828cd67a5a4f1e7a0bc713825abc98afaf2
boenkeijer <kebeijer@gmail.com>
2020-05-08 14:33:22 +0200

    updated page templates

---
+++ page-templates/about-us.php
@@ -0,0 +1,18 @@
+<?php
+  /* Template Name: About us */ 
+?>
+
+<?php get_header(); ?>
+
+<main class="whatwedo">  
+  	<section>  
+    	<h1><?php the_title(); ?></h1>
+    	<ul class="sub-page">
+			<li>Our mission</li>
+			<li>The team</li>
+			<li>History</li>
+		</ul>
+	</section>
+</main>
+
+<?php get_footer(); ?>
