dbc53e00d0acb121f51360376bba54c99685fcfa
boenkeijer <kebeijer@gmail.com>
2020-05-08 14:24:18 +0200

    Update whatwedo.php

--- page-templates/whatwedo.php
+++ page-templates/whatwedo.php
@@ -6,10 +6,12 @@
 
 <main class="whatwedo">  
   <section>  
-    <h1>1<?php the_title(); ?></h1>
+    <h1><?php the_title(); ?></h1>
     <ul class="sub-page">
         <li>Project 1</li>
         <li>Project 2</li>
+        <li>Project 3</li>
+        <li>Project 4</li>
     </ul>
   </section>
 </main>
