dbae855567405d9a989851766ad832debdc37f11
boenkeijer <kebeijer@gmail.com>
2020-05-08 16:46:14 +0200

    Go

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <head>
     <meta charset="utf-8">
-    <title>By Life Connected</title>
+    <title>By Life Connectedd</title>
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
