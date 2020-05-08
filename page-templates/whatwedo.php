ef679319afdac2be62a130391591452f3515739e
boenkeijer <kebeijer@gmail.com>
2020-05-08 13:54:18 +0200

    Create whatwedo.php

---
+++ page-templates/whatwedo.php
@@ -0,0 +1,17 @@
+<?php
+  /* Template Name: What we do */ 
+?>
+
+<?php get_header(); ?>
+
+<main class="contact">    
+    <section class="data">
+        <h1>What we do</h1>
+        <ul>
+            <li>Project 1</li>
+            <li>Project 2</li>
+        </ul>
+    </section>    
+</main>
+
+<?php get_footer(); ?>
