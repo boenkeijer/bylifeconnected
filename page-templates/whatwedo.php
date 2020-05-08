2af4d224be0472ea6803a4446ae8b9c059277d2b
boenkeijer <kebeijer@gmail.com>
2020-05-08 14:28:51 +0200

    Better title

--- page-templates/whatwedo.php
+++ page-templates/whatwedo.php
@@ -5,11 +5,14 @@
 <?php get_header(); ?>
 
 <main class="whatwedo">  
-  <section>  
-    <h1>Hallo</h1>
+  <section>
+    1  
+    <h1><?php the_title(); ?></h1>
     <ul class="sub-page">
         <li>Project 1</li>
         <li>Project 2</li>
+        <li>Project 3</li>
+        <li>Project 4</li>
     </ul>
   </section>
 </main>
