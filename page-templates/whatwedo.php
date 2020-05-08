a783e8f5598f2247095616bf3f79969576812fb3
boenkeijer <kebeijer@gmail.com>
2020-05-08 14:31:21 +0200

    Final whatwedo

--- page-templates/whatwedo.php
+++ page-templates/whatwedo.php
@@ -5,16 +5,14 @@
 <?php get_header(); ?>
 
 <main class="whatwedo">  
-  <section>
-    1  
-    <h1><?php the_title(); ?></h1>
-    <ul class="sub-page">
-        <li>Project 1</li>
-        <li>Project 2</li>
-        <li>Project 3</li>
-        <li>Project 4</li>
-    </ul>
-  </section>
+  	<section>  
+    	<h1><?php the_title(); ?></h1>
+    	<ul class="sub-page">
+			<li>Project 1</li>
+			<li>Project 2</li>
+			<li>Etc</li>
+		</ul>
+	</section>
 </main>
 
 <?php get_footer(); ?>
